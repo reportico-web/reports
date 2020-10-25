574fc732990b68fdd68993f156e938632fa9c137
Peter Deed <info@reportico.org>
2020-10-25 17:16:23 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * File:        index.php
+ *
+ * Top level index file resets to admin login page
+ *
+ * @link http://www.reportico.org/
+ * @copyright 2010-2014 Peter Deed
+ * @author Peter Deed <info@reportico.org>
+ * @package Reportico
+ * @version $Id: index.php,v 1.8 2014/05/17 15:12:31 peter Exp $
+ */
+
+// Include the Reportico library
+require __DIR__.'/start.php';
+
+?>
